015048ae43ba4d9e6f210bf714ddc9b44d039ae0
emmy <emmy66418@gmail.com>
2019-11-15 11:16:33 +0200

    feature(sign-in-api):a user sign in
    any user after signing up should login with the matching email and password
    
    [finishes:#169713619]

--- controller/user.js
+++ controller/user.js
@@ -3,35 +3,48 @@ import jwt from 'jsonwebtoken'
 
 class register{
    create = (req, res) => {
+      const email = model.findbyEmail(req.body.email);
+       if(!email){
        const newUser = model.createNewuser(req.body);
        const token = jwt.sign({id: newUser.id}, 'Kigali2010');
-       return res.status(201).send({
-          status: 201,
+       return res.status(200).send({
+          status: 200,
           message: "User created successfully",
           data: {
             token,
             newUser
           }
        });
-   }
-   signin = (req, res) =>{
-      const email = model.findbyOne(req.body.email)
-      if(email === false){
-         return res.status(401).send({
-            status: 401,
-            error: "email does not exist"
-         });
       }
-      if(email.findbyOne === req.body.email && password.findbypassword === req.body.password)
-      {
-         return res.status(201).send({
-            status:201,
-            message: "login successfully",
-            token: token,
-            data: email
-         })
-      }
+       return res.status(401).send({
+         status:401,
+         error:"email already exist"
+      })
    }
+ signin = (req, res) =>{
+    const find = model.findbyEmail(req.body.email)
+    if(!find){
+       return res.status(401).send({
+          status: 401,
+          error: "user doesn't exist"
+       })
+    }
+    if(find.email === req.body.email && find.password === req.body.password){
+      const token = jwt.sign({id: find.id}, 'Kigali2010');
+       return res.status(200).send({ 
+          status: 200,
+          message: "User is successfully logged in",
+          data: {
+             token: token,
+              find
+          }
+       });
+    }
+    return res.status(401).send({
+      status: 401,
+      error: "error occured"
+  })
+ }
 
 }
 export default new register();
